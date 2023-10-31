80c2b2f8268e99bccbac490deb23c7e273b92556
Nitzan <nitzantomer1998@gmail.com>
2023-10-31 09:10:24 +0200

    Initialize views folder

---
+++ base/views/__init__.py
@@ -0,0 +1,6 @@
+from .home import home
+from .topic import topics
+from .activity import activity
+from .message import messageDelete
+from .user import userRegistration, userLogin, userLogout, userProfile, userUpdateProfile
+from .room import room, roomRegistration, roomUpdate, roomDelete
