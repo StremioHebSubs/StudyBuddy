7a76301421880cdda1d5444866f620cf7b6d215e
Nitzan <nitzantomer1998@gmail.com>
2023-11-03 07:44:58 +0200

    Updating Docstring

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,14 @@
+"""
+Django Admin Configuration
+
+This module configures the Django admin panel for the StudyBuddy application.
+It registers the User, Room, Topic, and Message models to be managed through the admin interface.
+
+To access and manage these models via the Django admin panel, log in with appropriate admin credentials.
+
+For more information on Django admin, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/ref/contrib/admin/
+"""
 from django.contrib import admin
 
 from .models import User, Room, Topic, Message
