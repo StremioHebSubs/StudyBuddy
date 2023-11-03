b57838208646588fc392907f2044d581ad2079ca
Nitzan <nitzantomer1998@gmail.com>
2023-11-03 07:46:04 +0200

    Updating Docstring

--- base/forms/__init__.py
+++ base/forms/__init__.py
@@ -1,3 +1,11 @@
-from .user import UserRegistrationForm, UserLoginForm, UserUpdateForm
-from .room import RoomForm
+"""
+StudyBuddy Form Imports
+
+This module imports various forms used in the StudyBuddy Base application, including forms for user registration,
+user login, user profile updates, room creation, and message submission.
+
+The imported forms are part of the StudyBuddy application's user interface and data submission processes.
+"""
 from .message import MessageSubmitForm
+from .room import RoomForm
+from .user import UserRegistrationForm, UserLoginForm, UserUpdateForm
