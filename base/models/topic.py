43752badbf53f4947a65f7399f54b84c220e2f7a
Nitzan <nitzantomer1998@gmail.com>
2023-11-06 09:29:58 +0200

    Updating  Docstring

--- base/models/topic.py
+++ base/models/topic.py
@@ -1,14 +1,35 @@
+"""
+Topic Model for StudyBuddy
+
+This module defines the 'Topic' model for the StudyBuddy Base application. Topics are associated with a 'name' attribute
+and record the creation time using the 'created' field. The model allows for ordering topics based on their creation time.
+
+For more information on defining models with Django, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/db/models/
+"""
 from django.db import models
 
 
 class Topic(models.Model):
+    """
+    Topic Model for StudyBuddy
+
+    This module defines the 'Topic' model for the StudyBuddy Base application. Topics are associated with a 'name' attribute
+    and record the creation time using the 'created' field. The model allows for ordering topics based on their creation time.
+    """
     name = models.CharField(max_length=20, unique=True)
 
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['-created']
-
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return self.name.__str__()
     
+    class Meta:
+        """
+        Meta Configuration for the Topic Model
+
+        The 'Meta' class provides configuration options for the 'Topic' model.
+        It specifies the default ordering of topics based on their creation time.
+        Topics are ordered by the most recently created topics.
+        """
+        ordering = ['-created']
