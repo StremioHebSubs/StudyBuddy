16612d169a9f295aece94cb75ec04b4834fd1f98
Nitzan <nitzantomer1998@gmail.com>
2023-11-06 09:28:59 +0200

    Updating  Docstring

--- base/models/__init__.py
+++ base/models/__init__.py
@@ -1,4 +1,14 @@
-from .user import User
+"""
+StudyBuddy Models
+
+This module imports the various models used in the StudyBuddy Base application, including 'User,' 'Room,' 'Topic,' and 'Message.'
+These models represent key components of the StudyBuddy system and are used to store data related to users, chat rooms,
+topics, and messages.
+
+For more information on Django models, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/db/models/
+"""
+from .message import Message
 from .room import Room
 from .topic import Topic
-from .message import Message
+from .user import User
