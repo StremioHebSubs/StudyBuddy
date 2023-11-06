72de3269eec1cf19cc459a88d70403e60ded258d
Nitzan <nitzantomer1998@gmail.com>
2023-11-06 09:30:17 +0200

    Updating  Docstring

--- base/models/user.py
+++ base/models/user.py
@@ -1,8 +1,25 @@
+"""
+User Model for StudyBuddy
+
+This module defines the custom 'User' model for the StudyBuddy Base application, extending Django's 'AbstractUser' class.
+The 'User' model includes fields for 'name,' 'username,' 'email,' 'bio,' and 'avatar.' It also specifies the 'USERNAME_FIELD'
+as 'email' for authentication purposes.
+
+For more information on customizing user models with Django, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/auth/customizing/#using-a-custom-user-model
+"""
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """
+    User Model for StudyBuddy
+
+    This module defines the custom 'User' model for the StudyBuddy Base application, extending Django's 'AbstractUser' class.
+    The 'User' model includes fields for 'name,' 'username,' 'email,' 'bio,' and 'avatar.' It also specifies the 'USERNAME_FIELD'
+    as 'email' for authentication purposes.
+    """
     name = models.CharField(max_length=20, unique=True)
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
@@ -10,4 +27,4 @@ class User(AbstractUser):
     avatar = models.ImageField(default='default_avatar.jpg')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
