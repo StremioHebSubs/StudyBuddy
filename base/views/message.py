3d2b9db5a857239e6e8e80b77d1fa2ab428cc392
Nitzan <nitzantomer1998@gmail.com>
2023-11-07 08:22:44 +0200

    Updating Docstring

--- base/views/message.py
+++ base/views/message.py
@@ -1,3 +1,14 @@
+"""
+Message Deletion View
+
+This module defines a Django view for deleting messages. The view is protected by the `@login_required` decorator to ensure that
+only authenticated users can access it. Users can delete their own messages, and upon successful deletion, they are redirected to
+the room where the message was posted. Unauthorized users will be shown an error message.
+
+For more information on Django views, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/http/views/
+"""
+from typing import Union
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -6,7 +17,7 @@ from base.models import Message
 
 
 @login_required
-def messageDelete(request, pk):
+def messageDelete(request, pk) -> Union[render, redirect]:
     message = Message.objects.get(id=pk)
 
     if message is None:
@@ -14,7 +25,7 @@ def messageDelete(request, pk):
         return redirect('home')
     
     if request.user != message.user:
-        messages.error(request, f'You are not authorized to delete "{message.user.username}" message')
+        messages.error(request, f'You are not authorized to delete "{ message.user.username }" message')
         return redirect('room', message.room.pk)
     
     if request.method == 'POST':
