feef00495ee339b1e14cd67c9617066e1ad2a675
Nitzan <nitzantomer1998@gmail.com>
2023-11-07 08:23:03 +0200

    Updating Docstring

--- base/views/topic.py
+++ base/views/topic.py
@@ -1,13 +1,22 @@
+"""
+Topics View
+
+This module defines a Django view for displaying topics and their associated rooms. Users can search for topics, and the view lists topics
+matching the search query along with a count of rooms under each topic.
+
+For more information on Django views, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/http/views/
+"""
 from django.shortcuts import render
 
 from base.models import Room, Topic
 
 
-def topics(request):
+def topics(request) -> render:
     query = request.GET.get('query', '')
     
-    topics = Topic.objects.filter(name__icontains=query)
+    topicObjects = Topic.objects.filter(name__icontains=query)
     roomCounter = Room.objects.filter(topic__name__icontains=query).count()
     
-    context = {'topics': topics, 'roomCounter': roomCounter}
+    context = {'topics': topicObjects, 'roomCounter': roomCounter}
     return render(request, 'base/pages/topic.html', context)
