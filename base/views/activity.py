6211e891f336894fb4806e6efe3c3f6ef97fae96
Nitzan <nitzantomer1998@gmail.com>
2023-11-07 08:22:29 +0200

    Updating Docstring

--- base/views/activity.py
+++ base/views/activity.py
@@ -1,9 +1,18 @@
+"""
+Activity Page View
+
+This module defines a Django view for the "activity" page. The view retrieves all messages from the database and displays
+them on the activity page. This view is accessible to all users.
+
+For more information on Django views, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/http/views/
+"""
 from django.shortcuts import render
 
 from base.models import Message
 
 
-def activity(request):
+def activity(request) -> render:
     roomMessages = Message.objects.all()
 
     context = {'roomMessages': roomMessages}
