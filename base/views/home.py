2c678b52b6568fca741f6862088f0823902d09d8
Nitzan <nitzantomer1998@gmail.com>
2023-11-07 08:22:37 +0200

    Updating Docstring

--- base/views/home.py
+++ base/views/home.py
@@ -1,10 +1,20 @@
+"""
+Home Page View
+
+This module defines a Django view for the home page. The view retrieves a list of topics and allows users to search for rooms
+based on a query string. It displays topics and rooms matching the query and provides a count of matching rooms. This view is
+accessible to all users.
+
+For more information on Django views, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/http/views/
+"""
 from django.db.models import Q
 from django.shortcuts import render
 
-from base.models import Room, Topic, Message
+from base.models import Message, Room, Topic
 
 
-def home(request):
+def home(request) -> render:
     topics = Topic.objects.all()[0:5]
 
     roomQuery = request.GET.get('query', '')
