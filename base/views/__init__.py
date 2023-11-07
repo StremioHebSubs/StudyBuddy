821143847d83d23a0f0783e5f9e289a85fa84180
Nitzan <nitzantomer1998@gmail.com>
2023-11-07 08:22:22 +0200

    Updating Docstring

--- base/views/__init__.py
+++ base/views/__init__.py
@@ -1,6 +1,16 @@
-from .home import home
-from .topic import topics
+"""
+Django Views
+
+This module imports various views used in a Django project. These views handle different aspects of the application, 
+including displaying the home page, topic page, user registration and profile, room creation and management, activity feed, 
+and message handling. Each view is responsible for a specific part of the application's functionality.
+
+For more information on Django views, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/topics/http/views/
+"""
 from .activity import activity
+from .home import home
 from .message import messageDelete
-from .user import userRegistration, userLogin, userLogout, userProfile, userUpdateProfile
 from .room import room, roomRegistration, roomUpdate, roomDelete
+from .topic import topics
+from .user import userRegistration, userLogin, userLogout, userProfile, userUpdateProfile
