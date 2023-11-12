7e134e64202ae434b3cab9f95b3f2bb99a3dc2b2
Nitzan <nitzantomer1998@gmail.com>
2023-11-12 10:54:34 +0200

    Updating Condition

--- static/javascripts/script.js
+++ static/javascripts/script.js
@@ -2,7 +2,7 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 const dropdownButton = document.querySelector(".dropdown-button");
 const conversationThread = document.querySelector(".room__box");
 
-if (dropdownMenu)
+if (dropdownMenu && dropdownButton)
   dropdownButton.addEventListener("click", () => { dropdownMenu.classList.toggle("show"); });
 
 if (conversationThread)
