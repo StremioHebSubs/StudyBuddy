609c7f557bd50dc8edf814a4ba2316f4c29a752f
Nitzan <nitzantomer1998@gmail.com>
2023-11-12 10:53:28 +0200

    Updating Docstring

--- manage.py
+++ manage.py
@@ -1,3 +1,15 @@
+"""
+StudyBuddy Management Script
+
+This script is the main entry point for managing the StudyBuddy Django project.
+It sets up project settings and handles command-line execution.
+
+Usage:
+    python manage.py <management_command>
+
+For more information on available management commands, refer to the Django documentation:
+https://docs.djangoproject.com/en/stable/ref/django-admin/
+"""
 import os
 import sys
 from django.core.management import execute_from_command_line
